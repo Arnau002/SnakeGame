ba2d39915165c8e982248c6f5715ff39966cf9d1
Arnau002 <arnaucolomer94@gmail.com>
2021-11-29 16:46:50 +0100

    Versió 3.1 (la serp es mou, més o menys)

---
+++ Snake2/SnakeGame.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake2
+{
+    public enum DireccoSnake
+    {
+        Dreta,
+        Esquerra,
+        Avall,
+        Amunt
+    }
+
+    class SnakeGame
+    {
+        public const int X_SIZE = 20;
+        public const int Y_SIZE = 20;
+
+        Point capSerp = new Point(0, 0);
+        DireccoSnake direccio = DireccoSnake.Avall;
+
+        public Point CapSerp { get => capSerp; set => capSerp = value; }
+        public DireccoSnake Direccio { get => direccio; set => direccio = value; }
+
+        internal void moure()
+        {
+            capSerp.Y++;
+        }
+    }
+}
